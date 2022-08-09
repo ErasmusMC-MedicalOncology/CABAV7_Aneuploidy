a7255755ab4c8728af4519defce972e47dde83d5
job <j.vanriet@erasmusmc.nl>
2022-08-09 14:15:52 +0200

    Sync

--- R/1.CompareBaseline.R
+++ R/1.CompareBaseline.R
@@ -1,5 +1,5 @@
 # Author:                      Job van Riet
-# Date:                        07-07-2022
+# Date:                        09-08-2022
 # Function:                    Compare baseline characteristics of CABA-V7 and CABARESC.
 
 # Import libraries ----
@@ -26,19 +26,6 @@ data.Table1_CABARESC <- dplyr::bind_rows(
 )
 
 
-# Age
-# Dichotomized WHO
-# PSA at primary
-# Total Gleason
-# HBG
-# ALB
-# LDH
-# ALP
-# ANC
-# Z-scores
-# CTCs
-
-
 # Perform statistics. ----
 
 test.Table1 <- data.Table1 %>% 
@@ -60,7 +47,7 @@ test.Table1_CABARESC <- data.Table1_CABARESC %>%
 # Output table. ----
 
 data.Table1_CABARESC %>% 
-reshape2::melt(id.vars = 'cohort') %>%
+    reshape2::melt(id.vars = 'cohort') %>%
     dplyr::filter(variable != 'WHO/ECOG PS at registration') %>% 
     dplyr::group_by(cohort, variable) %>%
     dplyr::mutate(value = as.numeric(value)) %>% 
@@ -86,4 +73,42 @@ print(tableone::CreateTableOne(
     test = T
 ), showAllLevels = T)
 
-                  
+
+# Selection of relevant clinical characteristics. ----
+
+data.AIC <- readxl::read_xlsx('Misc./Suppl. Table 1 - Overview of Data.xlsx', sheet = 'Clinical (CABAV7)', trim_ws = T) %>% 
+    dplyr::inner_join(readxl::read_excel('Misc./Suppl. Table 1 - Overview of Data.xlsx', trim_ws = T, skip = 1, sheet = 'Overview (CABAV7)')) %>% 
+    # Convert dates.
+    dplyr::mutate_at(dplyr::vars(dplyr::contains('Date:')), as.Date) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+        dateCensor = ifelse(!is.na(`Date: Death`), `Date: Death`, na.omit(c(`Date: Last follow-up`, `Date: End of study`, `Date: Pre-screening`))),
+        dateCensor = as.Date(dateCensor, origin = '1970-01-01'),
+        daysFromPreScreeningToEnd = dateCensor - `Date: Pre-screening`,
+        monthsFromPreScreeningToEnd = daysFromPreScreeningToEnd / (365.25 / 12)
+    ) %>%
+    dplyr::ungroup() %>% 
+    dplyr::mutate(
+        `WHO status (Pooled)` = ifelse(`WHO/ECOG PS at registration` %in% c(1,2), '1 - 2', `WHO/ECOG PS at registration`),
+        `Dichotomized CTC count (Baseline)` = ifelse(`CTC Count (Baseline – 7.5mL)` >= 5, 'CTC Count ≥5', 'CTC Count <5'),
+    ) %>% 
+    dplyr::select(
+        `Age at registration`,
+        `WHO status (Pooled)`,
+        `PSA at primary diagnosis [ug/L]`,
+        HBG, ALB, LDH, ALP, ANC,
+        `Genome-Wide Z Score (Baseline)`,
+        `Dichotomized CTC count (Baseline)`,
+        Survival, monthsFromPreScreeningToEnd
+    ) %>% 
+    dplyr::filter(complete.cases(.))
+
+MASS::stepAIC(survival::coxph(formula = survival::Surv(monthsFromPreScreeningToEnd, Survival) ~ `Age at registration` + `WHO status (Pooled)` + `PSA at primary diagnosis [ug/L]` + HBG + ALB + 
+                                  LDH + ALP + ANC + `Genome-Wide Z Score (Baseline)` + `Dichotomized CTC count (Baseline)`, data = data.AIC, ties = 'breslow') , direction = 'backward')
+
+# Outcomes:
+#                                                   coef exp(coef)  se(coef)      z       p
+# `WHO status (Pooled)`1 - 2                       0.729202  2.073426  0.294403  2.477 0.01325
+# HBG                                             -0.507536  0.601977  0.190796 -2.660 0.00781
+# `Genome-Wide Z Score (Baseline)`                 0.012943  1.013027  0.004631  2.795 0.00519
+# `Dichotomized CTC count (Baseline)`CTC Count ≥5  1.082219  2.951223  0.342824  3.157 0.00160
